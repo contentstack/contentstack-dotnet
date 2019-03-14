bf279e50361c864238c5a40b0781a30435361315
Uttam K Ukkoji <uttamukkoji@gmail.com>
2019-03-14 11:57:36 +0530

    stack schema fetch implemented

--- Contentstack.Core.Tests/EntryTest.cs
+++ Contentstack.Core.Tests/EntryTest.cs
@@ -6,7 +6,8 @@ using Contentstack.Core.Configuration;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
-            
+using System.Collections;
+
 namespace Contentstack.Core.Tests
 {
    
@@ -30,6 +31,37 @@ namespace Contentstack.Core.Tests
                 Assert.False(true, "Entry.Fetch is not match with expected result.");
             } else {
                 Assert.True(result.Object.Count > 0 && result.EntryUid == sourceEntry.EntryUid && result.Object.ContainsKey("publish_details") && result.Object["publish_details"] != null);
+            }
+        }
+
+        [Fact]
+        public async Task GetContentTypes()
+        {
+            var result = await client.getContentTypes();
+
+            if (result == null)
+            {
+                Assert.False(true, "client.getContentTypes is not match with expected result.");
+            }
+            else
+            {
+                Assert.True(true);
+
+            }
+        }
+
+        [Fact]
+        public async Task FetchContenTypeSchema()
+        {
+            ContentType contenttype = client.ContentType(source);
+            var result = await contenttype.Fetch();
+            if (result == null)
+            {
+                Assert.False(true, "contenttype.FetchSchema() is not match with expected result.");
+            }
+            else
+            {
+                Assert.True(true);
             }
         }
 
