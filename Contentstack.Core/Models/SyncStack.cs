998b02d16cc55ffd085b309581029788398349d4
Uttam K Ukkoji <uttamukkoji@gmail.com>
2019-04-11 11:38:46 +0530

    Test cases refactor

---
+++ Contentstack.Core/Models/SyncStack.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Contentstack.Core.Models
+{
+    public class SyncStack
+    {
+        public IEnumerable<dynamic> items { get; set; }
+
+        public int skip { get; set; }
+
+        public int limit { get; set; }
+
+        public int total_count { get; set; }
+
+        public string sync_token { get; set; }
+
+        public string pagination_token { get; set; }
+
+    }
+}
