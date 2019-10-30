908c974df88827795d7ab3c17f8431ef4ce0f6c6
Uttam K Ukkoji <uttamukkoji@gmail.com>
2019-10-30 11:09:25 +0530

    Global field

--- Contentstack.Core.Tests/Models/SourceModelIncludeRef.cs
+++ Contentstack.Core.Tests/Models/SourceModelIncludeRef.cs
@@ -15,8 +15,8 @@ namespace Contentstack.Core.Tests.Models
         public Boolean Boolean;
         public string Date;
         public List<Entry> Reference;
-        //public List<string> Other_reference;
-        public List<Dictionary<string, object>> Other_reference;
+        public List<string> Other_reference;
+        // public List<Dictionary<string, object>> Other_reference;
         public Dictionary<string, object> Group;
         public List<Dictionary<string, object>> Modular_blocks;
 
