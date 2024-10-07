59f9adc0b2d177e63243a6480db8a6ff7044f19b
Nadeem Patwekar <nadeem.patwekar@contentstack.com>
2024-10-07 18:31:29 +0530

    fix: corrected uid to _uid in case of variants

--- Contentstack.Core.Tests/EntryTest.cs
+++ Contentstack.Core.Tests/EntryTest.cs
@@ -99,7 +99,7 @@ namespace Contentstack.Core.Tests
                     {
                         Assert.True(result.Uid == sourceEntry.Uid);
                         Assert.NotNull(result._variant);
-                        Assert.NotNull(result._variant["uid"]);
+                        Assert.NotNull(result._variant["_uid"]);
                     }
                 });
         }
@@ -123,7 +123,7 @@ namespace Contentstack.Core.Tests
                     {
                         Assert.True(result.Uid == sourceEntry.Uid);
                         Assert.NotNull(result._variant);
-                        Assert.NotNull(result._variant["uid"]);
+                        Assert.NotNull(result._variant["_uid"]);
                     }
                 });
         }
