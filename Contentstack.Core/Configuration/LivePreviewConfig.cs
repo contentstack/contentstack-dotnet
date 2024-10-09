983f66418a810be3044b8617afc1008ed59839e7
Nadeem Patwekar <nadeem.patwekar@contentstack.com>
2024-10-09 13:28:06 +0530

    feat: :sparkles: Live Preview 2.0 Implementation

--- Contentstack.Core/Configuration/LivePreviewConfig.cs
+++ Contentstack.Core/Configuration/LivePreviewConfig.cs
@@ -7,6 +7,7 @@ namespace Contentstack.Core.Configuration
     public class LivePreviewConfig
     {
         public string ManagementToken { get; set; }
+        public string PreviewToken { get; set; }
         public bool Enable { get; set; }
         public string Host { get; set; }
         internal string LivePreview { get; set; }
